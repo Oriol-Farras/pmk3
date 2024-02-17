85993b7ff2215747b3f184de871d321ad3b7c720
Oriol-Farras <oriolfarras2004@gmail.com>
2024-02-17 18:12:39 +0100

    Ciclic menu added

--- Menu.py
+++ Menu.py
@@ -1,18 +1,15 @@
 class Menu:
     def __init__(self, menu_list):
-        self.list = menu_list
-        self.__menu_dicc = {0:None}
+        self.__menu_list = menu_list
+        self.__menu_dicc = {0:""}
         self.__actual_position = 1
-        self.__submenu_dicc = {}
 
-        for i in self.list:
-            
-            if i != 0 or i <= self.list:
-                self.__menu_dicc[self.__actual_position] = self.list[self.__actual_position-1]
-                self.__submenu_dicc[self.__actual_position] = None
+        for i in self.__menu_list:
+            if i != 0 or i <= self.__menu_list:
+                self.__menu_dicc[self.__actual_position] = self.__menu_list[self.__actual_position-1]
                 self.__actual_position += 1
 
-        self.__menu_dicc[self.__actual_position + 1] = None
+        self.__menu_dicc[self.__actual_position] = ""
         self.__actual_position = 1
 
     # Change the first nun used value of the dict
@@ -21,53 +18,60 @@ class Menu:
 
     # Change the value of the last non used space of the dict
     def set_last_non_used_value(self, value):
-        self.__menu_dicc[self.length() + 1] = value
+        self.__menu_dicc[self.lenght() + 1] = value
 
     # Returns the length of the list
-    def length(self):
-        return len(self.list)
+    def lenght(self):
+        return len(self.__menu_list)
 
     # Iterate to the next element in the menu
     def next(self):
-        if self.__actual_position < self.length():
+        if self.__actual_position < self.lenght():
             self.__actual_position += 1
+        else:
+            self.__actual_position = 1
+
+    def __show_next(self):
+        if self.__actual_position < self.lenght():
+            return self.__actual_position + 1
+        else:
+            return 1
 
     # Iterate to the previous element in the menu
     def previous(self):
         if self.__actual_position > 1:
             self.__actual_position -= 1
+        else:
+            self.__actual_position = self.lenght()
+
+    def __show_previous(self):
+        if self.__actual_position > 1:
+            return self.__actual_position - 1
+        else:
+            return self.lenght()
 
     # Returns the actual value of the menu
     def get_value(self):
         return self.__menu_dicc.get(self.__actual_position)
-    
+
     # Returs the key related to the actual value
     def get_key(self):
         return list(self.__menu_dicc.keys())[self.__actual_position]
-        
+
     # Return a list whith the previous value, the actual value, and the next value
     def get_triplet_value(self):
         triplet_list = []
-        triplet_list.append(self.__menu_dicc.get(self.__actual_position - 1))
+        triplet_list.append(self.__menu_dicc.get(self.__show_previous()))
         triplet_list.append(self.__menu_dicc.get(self.__actual_position))
-        triplet_list.append(self.__menu_dicc.get(self.__actual_position + 1))
+        triplet_list.append(self.__menu_dicc.get(self.__show_next()))
 
         return triplet_list
 
     # Return a list whith the previous key, the actual key, and the next key
     def get_triplet_key(self):
         triplet_list = []
-        triplet_list.append(list(self.__menu_dicc.keys())[self.__actual_position - 1])
+        triplet_list.append(list(self.__menu_dicc.keys())[self.__show_previous()])
         triplet_list.append(list(self.__menu_dicc.keys())[self.__actual_position])
-        triplet_list.append(list(self.__menu_dicc.keys())[self.__actual_position + 1])
+        triplet_list.append(list(self.__menu_dicc.keys())[self.__show_next()])
 
         return triplet_list
-    
-    # Create a submenu for a key whith a menu
-    def set_submenu(self, key, menu_list):
-        self.__submenu_dicc[key] = Menu(menu_list)
-
-    # Returns the menu releted whit a key
-    def get_submenu(self, key):
-        return self.__submenu_dicc.get(key)
-
