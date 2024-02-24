7f7c06c64d5c87b2a85d6fe5d01205df314ced52
Oriol-Farras <oriolfarras2004@gmail.com>
2024-02-24 17:02:00 +0100

    main program

---
+++ main.py
@@ -0,0 +1,31 @@
+from Screen import Screen
+from gpiozero import Button
+from signal import pause
+
+def main():
+
+    # Pins dels bottons
+    up_button = Button(17)
+    ok_button = Button(19)
+    down_button = Button(11)
+
+    def up_say():
+        pass
+
+    def ok_say():
+        pass
+
+    def down_say():
+        pass
+
+    up_button.when_pressed = up_say
+    ok_button.when_pressed = ok_say
+    down_button.when_pressed = down_say
+
+    screen = Screen()
+
+    pause()
+        
+
+if __name__ == '__main__':
+    main()
