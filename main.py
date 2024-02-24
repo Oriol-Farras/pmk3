986bb1aa06953ba4d8e69388ebbee80d7a14c2eb
Oriol-Farras <oriolfarras2004@gmail.com>
2024-02-24 18:37:21 +0100

    reduction of unnecessary things

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 from Screen import Screen
-from gpiozero import Button
-from signal import pause
-
 
 def main():
-
     screen = Screen()
-    
-    pause()
-
 
 if __name__ == '__main__':
     main()
